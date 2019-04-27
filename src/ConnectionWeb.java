a784912278f7ffa807be230d27c3c621976a8ac6
lilbroadam <lilbroadam@gmail.com>
2019-04-26 21:11:29 -0500

    Restructured the code for how the Web is drawn
    New class called "WebDrawer":
    To draw a web:
    - Create a WebDrawer object that requires at minimum a List of People to
    be drawn
    - The window width, height, and the bubble size can be defined in the
    constructor or through a setter method
    - Pass true into webDrawer.setVisible(bool) to draw the Web

--- src/ConnectionWeb.java
+++ src/ConnectionWeb.java
@@ -1,13 +1,8 @@
-import java.awt.Canvas;
-import java.awt.Font;
-import java.awt.Graphics;
 import java.util.ArrayList;
-import javax.swing.JFrame;
 
 public class ConnectionWeb {
 
 	public static void main(String[] args) {
-		// TODO have the user prep the web
 		int width = 500;
 		int height = 500;
 		int bubbleSize = 50;
@@ -24,69 +19,11 @@ public class ConnectionWeb {
 		people.add(allen);
 		people.add(ana);
 		
-		Web.setUp(width, height, bubbleSize, adam, people);
-	}
-	
-	
-	private static class Web extends Canvas {
-		private static int width;
-		private static int height;
-		private static int bubbleSize;
-		private static int centerX;
-		private static int centerY;
-		private static Person mainPerson;
-		private static ArrayList<Person> population;
-		private static Graphics g;
-		
-		public static void setUp(int width, int height, int bubbleSize, Person mainPerson, ArrayList<Person> population) {
-			Web.width = width;
-			Web.height = height;
-			Web.bubbleSize = bubbleSize;
-			Web.centerX = width / 2;
-			Web.centerY = width/2;
-			Web.population = population;
-			Web.mainPerson = mainPerson;
-			
-			JFrame frame = new JFrame("Connection Web");
-			Canvas canvas = new Web();
-			canvas.setSize(width, height);
-			frame.add(canvas);
-//			frame.pack();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
-		}
-		
-		@Override
-		public void paint(Graphics g) {
-			Web.g = g;
-			
-			// draw the main person in the center first
-			int bubbleOffset = bubbleSize / 2;
-			drawBubble(mainPerson, centerX - bubbleOffset, centerY - bubbleOffset);
-			
-			for(int i = 0; i < population.size(); i++) {
-				if(population.get(i) != mainPerson) {
-					drawBubble(population.get(i), i * bubbleSize, i * bubbleSize);
-				}
-			}
-		}
-		
-		// x is the x coordinate of the top left of the bubble
-		// y is the y coordinate of the top left of the bubble
-		// TODO make a Bubble class to use as a wrapper class for Person but I forgot why
-		private void drawBubble(Person person, int x, int y) {
-			String name = person.getName();
-			
-			int nameWidth = g.getFontMetrics().stringWidth(name);
-			int nameHeight = g.getFontMetrics().getAscent();			
-			int nameXOffset = bubbleSize/2 - nameWidth/2;
-			int nameYOffset = bubbleSize/2 + nameHeight/2;
-			
-			g.setFont(g.getFont().deriveFont(Font.PLAIN, 14));
-			
-			g.drawOval(x, y, bubbleSize, bubbleSize);
-			g.drawString(name, x + nameXOffset, y + nameYOffset);
-		}
+//		WebDrawer webDrawer = new WebDrawer(people);
+		WebDrawer webDrawer = new WebDrawer(people, width, height, bubbleSize);
+		webDrawer.setVisible(true);
+//		webDrawer.setWindowSize(width, height);
+//		webDrawer.setBubbleSize(bubbleSize);
 	}
 	
 }
